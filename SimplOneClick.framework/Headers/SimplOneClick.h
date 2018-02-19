37771d1383ac02cde2e25351f14252d8d865fa5b
Devarajan Narayanan <dvrajan12@gmail.com>
2018-02-19 12:05:49 +0530

    Updated library

--- SimplOneClick.framework/Headers/SimplOneClick.h
+++ SimplOneClick.framework/Headers/SimplOneClick.h
@@ -6,14 +6,9 @@
 //  Copyright © 2018 Simpl. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 
-//! Project version number for SimplOneClick.
-FOUNDATION_EXPORT double SimplOneClickVersionNumber;
-
-//! Project version string for SimplOneClick.
-FOUNDATION_EXPORT const unsigned char SimplOneClickVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SimplOneClick/PublicHeader.h>
-
+#import "GSManager.h"
+#import "GSUser.h"
+#import "GSTransaction.h"
+#import "GSManager+Transaction.h"
 
