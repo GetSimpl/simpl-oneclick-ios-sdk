6ec47b5a365fe3f9eaa7d66beb01ebb00b5671be
Devarajan Narayanan <dvrajan12@gmail.com>
2018-02-19 12:32:51 +0530

    Updated Library

--- SimplOneClick.framework/Headers/SimplOneClick.h
+++ SimplOneClick.framework/Headers/SimplOneClick.h
@@ -5,6 +5,7 @@
 //  Created by Devarajan on 30/01/18.
 //  Copyright © 2018 Simpl. All rights reserved.
 //
+#import <UIKit/UIKit.h>
 
 //! Project version number for SimplOneClick.
 FOUNDATION_EXPORT double SimplOneClickVersionNumber;
