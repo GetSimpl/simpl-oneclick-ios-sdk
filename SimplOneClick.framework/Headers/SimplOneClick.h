1a723c7f6b924d49cef217579620e10a156c0e2e
Devarajan Narayanan <devarajan@getsimpl.com>
2018-05-08 17:49:09 +0530

    Release v1.0.4

--- SimplOneClick.framework/Headers/SimplOneClick.h
+++ SimplOneClick.framework/Headers/SimplOneClick.h
@@ -2,9 +2,10 @@
 //  SimplOneClick.h
 //  SimplOneClick
 //
-//  Created by Devarajan on 30/01/18.
+//  Created by Devarajan on 08/05/18.
 //  Copyright © 2018 Simpl. All rights reserved.
 //
+
 #import <UIKit/UIKit.h>
 
 //! Project version number for SimplOneClick.
@@ -13,8 +14,9 @@ FOUNDATION_EXPORT double SimplOneClickVersionNumber;
 //! Project version string for SimplOneClick.
 FOUNDATION_EXPORT const unsigned char SimplOneClickVersionString[];
 
+// In this header, you should import all the public headers of your framework using statements like #import <SimplOneClick/PublicHeader.h>
+
 #import "GSManager.h"
 #import "GSUser.h"
 #import "GSTransaction.h"
 #import "GSManager+Transaction.h"
-
